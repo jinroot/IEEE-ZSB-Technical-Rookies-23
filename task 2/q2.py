60310174680a9cf2af03e6d6e522308cd7a69147
John Ashraf <84095010+jinroot@users.noreply.github.com>
2022-11-28 11:01:15 +0200

    Delete task 2 directory

--- task 2/q2.py
+++
@@ -1,6 +0,0 @@
-
-input = input("input: ")
-if input == input[::-1]:
-    print("yes")
-else:
-    print("no")
--- task 2/q4.py
+++
@@ -1,6 +0,0 @@
-input = input()
-list = input.split()
-
-noDubs = [*set(list)]
-noDubs.sort()
-print(noDubs)
